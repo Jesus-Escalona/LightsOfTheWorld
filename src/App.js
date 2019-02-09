fed07d22f482a3c370824235fccf0fd0c11a1612
Jesus Escalona <jesusescalona@me.com>
2019-02-09 18:08:27 -0500

    forgot to commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Fragment, Component } from 'react';
+import {Container} from "semantic-ui-react";
 import './App.css';
+import Plot from 'react-plotly.js';
+import states from "./states";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
+const getNames = (key) => states.map(state => state[key])
+const getRand = (n) => {
+    let arr = []
+    for(let i = 0; i < n; i++) {
+        let number = Math.floor(Math.random()*10);
+        arr.push(number)
+    }
+    return arr
+}
+const data = [
+    {
+        type: 'choropleth',
+        locationmode: 'USA-states',
+        locations: getNames("abbreviation"),
+        text: getNames("name"),
+        z: getRand(59),
+        autocolorscale: true
+    }
+];
+
+const layout = {
+    title: 'USA MAP',
+    autosize: true,
+    geo: {
+        scope: 'usa',
+        showlakes: true,
+        subunitcolor: '#060104'
+    }
+};
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Fragment>
+            <NavBar />
+            <Container fluid align="center">
+                <Plot
+                    data={data}
+                    layout={layout}
+                    useResizeHandler
+                    style={{ width: '100%', height: '100%' }}
+                />
+            </Container>
+            <Footer />
+        </Fragment>
     );
   }
 }
---
+++ src/containers/DesktopContainer.js
@@ -0,0 +1,64 @@
+import {Menu, Button, Container, Responsive, Segment, Visibility, Image} from "semantic-ui-react";
+import React, {Component} from "react";
+
+const getWidth = () => {
+    const isSSR = typeof window === 'undefined'
+
+    return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
+}
+
+export default class DesktopContainer extends Component {
+    state = {}
+
+    hideFixedMenu = () => this.setState({ fixed: false })
+    showFixedMenu = () => this.setState({ fixed: true })
+
+    render() {
+        const { children } = this.props
+        const { fixed } = this.state
+
+        return (
+            <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
+                <Visibility
+                    once={false}
+                    onBottomPassed={this.showFixedMenu}
+                    onBottomPassedReverse={this.hideFixedMenu}
+                >
+                    <Segment
+                        inverted
+                        textAlign='center'
+                        style={{ minHeight: 100, padding: '1em 0em' }}
+                        vertical
+                    >
+                        <Menu
+                            fixed={fixed ? 'top' : null}
+                            inverted={!fixed}
+                            pointing={!fixed}
+                            secondary={!fixed}
+                            size='large'
+                        >
+                            <Container>
+                                <Menu.Item as='a'>
+                                    <Image src={require('../assets/images/logo.png')} size='mini'/>
+                                </Menu.Item>
+                                <Menu.Item as='h2' active>
+                                    Emotional State
+                                </Menu.Item>
+                                <Menu.Item position='right'>
+                                    <Button as='a' inverted={!fixed}>
+                                        Log in
+                                    </Button>
+                                    <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
+                                        Sign Up
+                                    </Button>
+                                </Menu.Item>
+                            </Container>
+                        </Menu>
+                    </Segment>
+                </Visibility>
+
+                {children}
+            </Responsive>
+        )
+    }
+}
